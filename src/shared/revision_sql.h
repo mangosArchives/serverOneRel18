d11fe61ece41feecf64fca033577a633edcccc01
billy1arm <antz@getmangos.co.uk>
2013-06-28 11:47:26 +0100

    [m2048] sync'd realmd version with other cores

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s1350_11716_09_characters_mail"
  #define REVISION_DB_MANGOS "required_s2034_12522_01_mangos_db_script_string"
- #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
+ #define REVISION_DB_REALMD "required_c12484_02_realmd_account_access"
 #endif // __REVISION_SQL_H__
