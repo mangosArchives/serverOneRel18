909fed4e913e15e1514a3fa58e201e37d5fd1570
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-11-10 09:58:52 +0100

    [Scripts] Merged the script library based on version 2747
    This commit may result in a merge conflict if you already have
    an existing checkout of the script library. If so, delete it
    prior to pulling this update.
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

---
+++ src/bindings/scripts/scripts/eastern_kingdoms/karazhan/instance_karazhan.cpp
@@ -0,0 +1,351 @@
+/* Copyright (C) 2006 - 2013 ScriptDev2 <http://www.scriptdev2.com/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* ScriptData
+SDName: Instance_Karazhan
+SD%Complete: 70
+SDComment: Instance Script for Karazhan to help in various encounters.
+SDCategory: Karazhan
+EndScriptData */
+
+#include "precompiled.h"
+#include "karazhan.h"
+
+/*
+0  - Attumen + Midnight (optional)
+1  - Moroes
+2  - Maiden of Virtue (optional)
+3  - Opera Event
+4  - Curator
+5  - Terestian Illhoof (optional)
+6  - Shade of Aran (optional)
+7  - Netherspite (optional)
+8  - Chess Event
+9  - Prince Malchezzar
+10 - Nightbane
+*/
+
+instance_karazhan::instance_karazhan(Map* pMap) : ScriptedInstance(pMap),
+    m_uiOperaEvent(0),
+    m_uiOzDeathCount(0)
+{
+    Initialize();
+}
+
+void instance_karazhan::Initialize()
+{
+    memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+}
+
+bool instance_karazhan::IsEncounterInProgress() const
+{
+    for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+    {
+        if (m_auiEncounter[i] == IN_PROGRESS)
+            return true;
+    }
+
+    return false;
+}
+
+void instance_karazhan::OnPlayerEnter(Player* /*pPlayer*/)
+{
+    // If the opera event is already set, return
+    if (GetData(TYPE_OPERA_PERFORMANCE) != 0)
+        return;
+
+    // Set the Opera Performance type on the first player enter
+    SetData(TYPE_OPERA_PERFORMANCE, urand(OPERA_EVENT_WIZARD_OZ, OPERA_EVENT_ROMULO_AND_JUL));
+}
+
+void instance_karazhan::OnCreatureCreate(Creature* pCreature)
+{
+    switch (pCreature->GetEntry())
+    {
+        case NPC_ATTUMEN:
+        case NPC_MIDNIGHT:
+        case NPC_MOROES:
+        case NPC_BARNES:
+        case NPC_NIGHTBANE:
+        case NPC_JULIANNE:
+        case NPC_ROMULO:
+        case NPC_LADY_KEIRA_BERRYBUCK:
+        case NPC_LADY_CATRIONA_VON_INDI:
+        case NPC_LORD_CRISPIN_FERENCE:
+        case NPC_BARON_RAFE_DREUGER:
+        case NPC_BARONESS_DOROTHEA_MILLSTIPE:
+        case NPC_LORD_ROBIN_DARIS:
+        case NPC_IMAGE_OF_MEDIVH:
+        case NPC_IMAGE_OF_ARCANAGOS:
+            m_mNpcEntryGuidStore[pCreature->GetEntry()] = pCreature->GetObjectGuid();
+            break;
+        case NPC_NIGHTBANE_HELPER:
+            if (pCreature->GetPositionZ() < 100.0f)
+                m_lNightbaneGroundTriggers.push_back(pCreature->GetObjectGuid());
+            else
+                m_lNightbaneAirTriggers.push_back(pCreature->GetObjectGuid());
+            break;
+    }
+}
+
+void instance_karazhan::OnObjectCreate(GameObject* pGo)
+{
+    switch (pGo->GetEntry())
+    {
+        case GO_STAGE_DOOR_LEFT:
+        case GO_STAGE_DOOR_RIGHT:
+            if (m_auiEncounter[3] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            break;
+        case GO_GAMESMANS_HALL_EXIT_DOOR:
+            if (m_auiEncounter[8] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            break;
+        case GO_SIDE_ENTRANCE_DOOR:
+            if (m_auiEncounter[3] == DONE)
+                pGo->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_LOCKED);
+            break;
+        case GO_STAGE_CURTAIN:
+        case GO_PRIVATE_LIBRARY_DOOR:
+        case GO_MASSIVE_DOOR:
+        case GO_GAMESMANS_HALL_DOOR:
+        case GO_NETHERSPACE_DOOR:
+        case GO_DUST_COVERED_CHEST:
+        case GO_MASTERS_TERRACE_DOOR_1:
+        case GO_MASTERS_TERRACE_DOOR_2:
+            break;
+
+            // Opera event backgrounds
+        case GO_OZ_BACKDROP:
+        case GO_HOOD_BACKDROP:
+        case GO_HOOD_HOUSE:
+        case GO_RAJ_BACKDROP:
+        case GO_RAJ_MOON:
+        case GO_RAJ_BALCONY:
+            break;
+        case GO_OZ_HAY:
+            m_lOperaHayGuidList.push_back(pGo->GetObjectGuid());
+            return;
+        case GO_HOOD_TREE:
+            m_lOperaTreeGuidList.push_back(pGo->GetObjectGuid());
+            return;
+
+        default:
+            return;
+    }
+    m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+}
+
+void instance_karazhan::SetData(uint32 uiType, uint32 uiData)
+{
+    switch (uiType)
+    {
+        case TYPE_ATTUMEN:
+            m_auiEncounter[uiType] = uiData;
+            if (uiData == FAIL)
+            {
+                // Respawn Midnight on Fail
+                if (Creature* pMidnight = GetSingleCreatureFromStorage(NPC_MIDNIGHT))
+                {
+                    if (!pMidnight->isAlive())
+                        pMidnight->Respawn();
+                }
+            }
+            break;
+        case TYPE_MOROES:
+        case TYPE_MAIDEN:
+            m_auiEncounter[uiType] = uiData;
+            break;
+        case TYPE_OPERA:
+            // Don't store the same data twice
+            if (uiData == m_auiEncounter[uiType])
+                break;
+            m_auiEncounter[uiType] = uiData;
+            if (uiData == IN_PROGRESS)
+                m_uiOzDeathCount = 0;
+            if (uiData == DONE)
+            {
+                DoUseDoorOrButton(GO_STAGE_DOOR_LEFT);
+                DoUseDoorOrButton(GO_STAGE_DOOR_RIGHT);
+                DoToggleGameObjectFlags(GO_SIDE_ENTRANCE_DOOR, GO_FLAG_LOCKED, false);
+            }
+            // use curtain only for event start or fail
+            else
+                DoUseDoorOrButton(GO_STAGE_CURTAIN);
+            break;
+        case TYPE_CURATOR:
+        case TYPE_TERESTIAN:
+            m_auiEncounter[uiType] = uiData;
+            break;
+        case TYPE_ARAN:
+            if (uiData == FAIL || uiData == DONE)
+                DoToggleGameObjectFlags(GO_PRIVATE_LIBRARY_DOOR, GO_FLAG_LOCKED, false);
+            if (uiData == IN_PROGRESS)
+                DoToggleGameObjectFlags(GO_PRIVATE_LIBRARY_DOOR, GO_FLAG_LOCKED, true);
+            m_auiEncounter[uiType] = uiData;
+            break;
+        case TYPE_NETHERSPITE:
+            m_auiEncounter[uiType] = uiData;
+            DoUseDoorOrButton(GO_MASSIVE_DOOR);
+            break;
+        case TYPE_CHESS:
+            if (uiData == DONE)
+            {
+                DoUseDoorOrButton(GO_GAMESMANS_HALL_EXIT_DOOR);
+                DoRespawnGameObject(GO_DUST_COVERED_CHEST, DAY);
+            }
+            m_auiEncounter[uiType] = uiData;
+            break;
+        case TYPE_MALCHEZZAR:
+            DoUseDoorOrButton(GO_NETHERSPACE_DOOR);
+            m_auiEncounter[uiType] = uiData;
+            break;
+        case TYPE_NIGHTBANE:
+            m_auiEncounter[uiType] = uiData;
+            DoUseDoorOrButton(GO_MASTERS_TERRACE_DOOR_1);
+            DoUseDoorOrButton(GO_MASTERS_TERRACE_DOOR_2);
+            break;
+            // Store the event type for the Opera
+        case TYPE_OPERA_PERFORMANCE:
+            m_uiOperaEvent = uiData;
+            break;
+    }
+
+    // Also save the opera performance, once it's set
+    if (uiData == DONE || uiType == TYPE_OPERA_PERFORMANCE)
+    {
+        OUT_SAVE_INST_DATA;
+
+        std::ostringstream saveStream;
+        saveStream << m_auiEncounter[0] << " " << m_auiEncounter[1] << " " << m_auiEncounter[2] << " "
+                   << m_auiEncounter[3] << " " << m_auiEncounter[4] << " " << m_auiEncounter[5] << " "
+                   << m_auiEncounter[6] << " " << m_auiEncounter[7] << " " << m_auiEncounter[8] << " "
+                   << m_auiEncounter[9] << " " << m_auiEncounter[10] << " " << m_uiOperaEvent;
+
+        m_strInstData = saveStream.str();
+
+        SaveToDB();
+        OUT_SAVE_INST_DATA_COMPLETE;
+    }
+}
+
+uint32 instance_karazhan::GetData(uint32 uiType) const
+{
+    if (uiType < MAX_ENCOUNTER)
+        return m_auiEncounter[uiType];
+
+    if (uiType == TYPE_OPERA_PERFORMANCE)
+        return m_uiOperaEvent;
+
+    return 0;
+}
+
+void instance_karazhan::Load(const char* chrIn)
+{
+    if (!chrIn)
+    {
+        OUT_LOAD_INST_DATA_FAIL;
+        return;
+    }
+
+    OUT_LOAD_INST_DATA(chrIn);
+
+    std::istringstream loadStream(chrIn);
+
+    loadStream >> m_auiEncounter[0] >> m_auiEncounter[1] >> m_auiEncounter[2] >> m_auiEncounter[3]
+               >> m_auiEncounter[4] >> m_auiEncounter[5] >> m_auiEncounter[6] >> m_auiEncounter[7]
+               >> m_auiEncounter[8] >> m_auiEncounter[9] >> m_auiEncounter[10] >> m_uiOperaEvent;
+
+    for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+    {
+        if (m_auiEncounter[i] == IN_PROGRESS)               // Do not load an encounter as "In Progress" - reset it instead.
+            m_auiEncounter[i] = NOT_STARTED;
+    }
+
+    OUT_LOAD_INST_DATA_COMPLETE;
+}
+
+void instance_karazhan::OnCreatureDeath(Creature* pCreature)
+{
+    switch (pCreature->GetEntry())
+    {
+        case NPC_DOROTHEE:
+        case NPC_ROAR:
+        case NPC_TINHEAD:
+        case NPC_STRAWMAN:
+            ++m_uiOzDeathCount;
+            // Summon Chrone when all 4 Oz mobs are killed
+            if (m_uiOzDeathCount == MAX_OZ_OPERA_MOBS)
+            {
+                if (Creature* pCrone = pCreature->SummonCreature(NPC_CRONE, afChroneSpawnLoc[0], afChroneSpawnLoc[1], afChroneSpawnLoc[2], afChroneSpawnLoc[3], TEMPSUMMON_DEAD_DESPAWN, 0))
+                {
+                    if (pCreature->getVictim())
+                        pCrone->AI()->AttackStart(pCreature->getVictim());
+                }
+            }
+            break;
+    }
+}
+
+void instance_karazhan::DoPrepareOperaStage(Creature* pOrganizer)
+{
+    if (!pOrganizer)
+        return;
+
+    debug_log("SD2: Barnes Opera Event - Introduction complete - preparing encounter %d", GetData(TYPE_OPERA_PERFORMANCE));
+
+    // summon the bosses and respawn the stage background
+    switch (GetData(TYPE_OPERA_PERFORMANCE))
+    {
+        case OPERA_EVENT_WIZARD_OZ:
+            for (uint8 i = 0; i < MAX_OZ_OPERA_MOBS; ++i)
+                pOrganizer->SummonCreature(aOperaLocOz[i].uiEntry, aOperaLocOz[i].fX, aOperaLocOz[i].fY, aOperaLocOz[i].fZ, aOperaLocOz[i].fO, TEMPSUMMON_DEAD_DESPAWN, 0);
+            DoRespawnGameObject(GO_OZ_BACKDROP, 12 * HOUR);
+            for (GuidList::const_iterator itr = m_lOperaHayGuidList.begin(); itr != m_lOperaHayGuidList.end(); ++itr)
+                DoRespawnGameObject(*itr, 12 * HOUR);
+            break;
+        case OPERA_EVENT_RED_RIDING_HOOD:
+            pOrganizer->SummonCreature(aOperaLocWolf.uiEntry, aOperaLocWolf.fX, aOperaLocWolf.fY, aOperaLocWolf.fZ, aOperaLocWolf.fO, TEMPSUMMON_DEAD_DESPAWN, 0);
+            DoRespawnGameObject(GO_HOOD_BACKDROP, 12 * HOUR);
+            DoRespawnGameObject(GO_HOOD_HOUSE,    12 * HOUR);
+            for (GuidList::const_iterator itr = m_lOperaTreeGuidList.begin(); itr != m_lOperaTreeGuidList.end(); ++itr)
+                DoRespawnGameObject(*itr, 12 * HOUR);
+            break;
+        case OPERA_EVENT_ROMULO_AND_JUL:
+            pOrganizer->SummonCreature(aOperaLocJul.uiEntry, aOperaLocJul.fX, aOperaLocJul.fY, aOperaLocJul.fZ, aOperaLocJul.fO, TEMPSUMMON_DEAD_DESPAWN, 0);
+            DoRespawnGameObject(GO_RAJ_BACKDROP, 12 * HOUR);
+            DoRespawnGameObject(GO_RAJ_MOON,     12 * HOUR);
+            DoRespawnGameObject(GO_RAJ_BALCONY,  12 * HOUR);
+            break;
+    }
+
+    SetData(TYPE_OPERA, IN_PROGRESS);
+}
+
+InstanceData* GetInstanceData_instance_karazhan(Map* pMap)
+{
+    return new instance_karazhan(pMap);
+}
+
+void AddSC_instance_karazhan()
+{
+    Script* pNewScript;
+
+    pNewScript = new Script;
+    pNewScript->Name = "instance_karazhan";
+    pNewScript->GetInstanceData = &GetInstanceData_instance_karazhan;
+    pNewScript->RegisterSelf();
+}
---
+++ src/bindings/scripts/scripts/eastern_kingdoms/sunwell_plateau/sunwell_plateau.h
@@ -0,0 +1,127 @@
+/* Copyright (C) 2006 - 2013 ScriptDev2 <http://www.scriptdev2.com/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_SUNWELLPLATEAU_H
+#define DEF_SUNWELLPLATEAU_H
+
+enum
+{
+    MAX_ENCOUNTER               = 6,
+
+    TYPE_KALECGOS               = 0,
+    TYPE_BRUTALLUS              = 1,
+    TYPE_FELMYST                = 2,
+    TYPE_EREDAR_TWINS           = 3,
+    TYPE_MURU                   = 4,
+    TYPE_KILJAEDEN              = 5,
+
+    NPC_KALECGOS_DRAGON         = 24850,            // kalecgos blue dragon hostile
+    NPC_KALECGOS_HUMAN          = 24891,            // kalecgos human form in spectral realm
+    NPC_SATHROVARR              = 24892,
+    NPC_MADRIGOSA               = 24895,
+    NPC_FLIGHT_TRIGGER_LEFT     = 25357,            // Related to Felmyst flight path. Also the anchor to summon Madrigosa
+    NPC_FLIGHT_TRIGGER_RIGHT    = 25358,            // related to Felmyst flight path
+    NPC_WORLD_TRIGGER           = 22515,
+    NPC_BRUTALLUS               = 24882,
+    NPC_FELMYST                 = 25038,
+    NPC_ALYTHESS                = 25166,
+    NPC_SACROLASH               = 25165,
+    NPC_MURU                    = 25741,
+    NPC_ENTROPIUS               = 25840,
+    NPC_DECEIVER                = 25588,
+    NPC_KILJAEDEN               = 25315,
+    NPC_KILJAEDEN_CONTROLLER    = 25608,            // kiljaeden event controller
+    NPC_ANVEENA                 = 26046,            // related to kiljaeden event
+    NPC_KALECGOS                = 25319,            // related to kiljaeden event
+    NPC_VELEN                   = 26246,
+    NPC_LIADRIN                 = 26247,
+
+    GO_FORCEFIELD               = 188421,           // kalecgos door + collisions
+    GO_BOSS_COLLISION_1         = 188523,
+    GO_BOSS_COLLISION_2         = 188524,
+    GO_ICE_BARRIER              = 188119,           // used to block the players path during the Brutallus intro event
+    GO_FIRE_BARRIER             = 188075,           // door after felmyst
+    GO_FIRST_GATE               = 187766,           // door between felmyst and eredar twins
+    GO_SECOND_GATE              = 187764,           // door after eredar twins
+    GO_MURU_ENTER_GATE          = 187990,           // muru gates
+    GO_MURU_EXIT_GATE           = 188118,
+    GO_THIRD_GATE               = 187765,           // door after muru; why another?
+
+    GO_ORB_BLUE_FLIGHT_1        = 187869,           // orbs used in the Kil'jaeden fight
+    GO_ORB_BLUE_FLIGHT_2        = 188114,
+    GO_ORB_BLUE_FLIGHT_3        = 188115,
+    GO_ORB_BLUE_FLIGHT_4        = 188116,
+
+    SAY_KALECGOS_OUTRO          = -1580043,
+    SAY_TWINS_INTRO             = -1580044,
+
+    // Kil'jaeden yells
+    SAY_ORDER_1                 = -1580064,
+    SAY_ORDER_2                 = -1580065,
+    SAY_ORDER_3                 = -1580066,
+    SAY_ORDER_4                 = -1580067,
+    SAY_ORDER_5                 = -1580068,
+
+    AREATRIGGER_TWINS           = 4937,
+
+    // Felmyst ouro spell
+    SPELL_OPEN_BACK_DOOR        = 46650,            // Opens the fire barrier - script effect for 46652
+    // used by both muru and entropius
+    SPELL_MURU_BERSERK          = 26662,
+    // visuals for Kiljaeden encounter
+    SPELL_ANVEENA_DRAIN         = 46410,
+
+    MAX_DECEIVERS               = 3
+};
+
+// Used to summon Felmyst in reload case. This is the place where Madrigosa is killed in the intro event
+static const float aMadrigosaGroundLoc[4] = {1459.35f, 636.81f, 19.94f, 4.88f};
+// Used as an anchor to move Madrigosa and Kalec during the cinematic
+static const float aMadrigosaFlyLoc[3] = {1459.35f, 636.81f, 59.234f};
+
+class MANGOS_DLL_DECL instance_sunwell_plateau : public ScriptedInstance, private DialogueHelper
+{
+    public:
+        instance_sunwell_plateau(Map* pMap);
+        ~instance_sunwell_plateau() {}
+
+        void Initialize() override;
+        bool IsEncounterInProgress() const override;
+
+        void OnPlayerEnter(Player* pPlayer) override;
+        void OnObjectCreate(GameObject* pGo) override;
+        void OnCreatureCreate(Creature* pCreature) override;
+        void OnCreatureDeath(Creature* pCreature) override;
+        void OnCreatureEvade(Creature* pCreature);
+
+        void SetData(uint32 uiType, uint32 uiData) override;
+        uint32 GetData(uint32 uiType) const override;
+
+        void Update(uint32 uiDiff) override;
+
+        ObjectGuid SelectFelmystFlightTrigger(bool bLeftSide, uint8 uiIndex);
+
+        const char* Save() const override { return m_strInstData.c_str(); }
+        void Load(const char* chrIn) override;
+
+    protected:
+        void JustDidDialogueStep(int32 iEntry) override;
+        void DoSortFlightTriggers();
+
+        uint32 m_auiEncounter[MAX_ENCOUNTER];
+        std::string m_strInstData;
+
+        // Misc
+        uint8 m_uiDeceiversKilled;
+        uint32 m_uiSpectralRealmTimer;
+        uint32 m_uiKalecRespawnTimer;
+        uint32 m_uiMuruBerserkTimer;
+        uint32 m_uiKiljaedenYellTimer;
+
+        GuidVector m_vRightFlightTriggersVect;
+        GuidVector m_vLeftFlightTriggersVect;
+        GuidList m_lAllFlightTriggersList;
+        GuidList m_lDeceiversGuidList;
+};
+#endif
