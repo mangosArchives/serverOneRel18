744c25259ea8e0a5a072b096bb974599124651ba
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-11-16 16:53:59 +0100

    [Core] Fixed a compiler warning.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/game/Formulas.h
+++ src/game/Formulas.h
@@ -32,7 +32,7 @@ namespace MaNGOS
     }
     namespace XP
     {
-        enum XPColorChar { RED, ORANGE, YELLOW, GREEN, GRAY };
+        typedef enum XPColorChar { RED, ORANGE, YELLOW, GREEN, GRAY } XPColorChar;
 
         inline uint32 GetGrayLevel(uint32 pl_level)
         {
