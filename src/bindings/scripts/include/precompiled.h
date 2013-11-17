469c365e2db83f2efbdcff6f181646ea33815555
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-11-17 11:07:31 +0100

    [Build] Windows build fix for script library.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/bindings/scripts/include/precompiled.h
+++ src/bindings/scripts/include/precompiled.h
@@ -38,12 +38,4 @@
 #include "sc_instance.h"
 #include "SpellAuras.h"
 
-#ifdef WIN32
-#  include <windows.h>
-BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
-{
-    return true;
-}
-#endif
-
 #endif
