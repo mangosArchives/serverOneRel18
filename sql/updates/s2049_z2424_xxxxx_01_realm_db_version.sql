0bd724f27c884d6354c013267bd3782ba37a8989
Antz <admin@getmangos.co.uk>
2013-12-31 09:43:53 +0000

    [m] Updated SQL update column name

--- sql/updates/s2049_z2424_xxxxx_01_realm_db_version.sql
+++ sql/updates/s2049_z2424_xxxxx_01_realm_db_version.sql
@@ -1,1 +1,1 @@
-ALTER TABLE realmd_db_version CHANGE COLUMN required_10008_01_realmd_realmd_db_version required_c12484_02_realmd_account_access BIT;
+ALTER TABLE realmd_db_version CHANGE COLUMN s2034_12522_01_mangos_db_script_string required_c12484_02_realmd_account_access BIT;
