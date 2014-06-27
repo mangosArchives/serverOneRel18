0161b9550dc3c45f4083a9ef171663b131059dff
Antz <billy1arm@users.noreply.github.com>
2014-06-27 22:34:34 +0100

    [Scripts] Script files reorg

--- src/scripts/ScriptDevMgr.cpp
+++ src/scripts/ScriptDevMgr.cpp
@@ -644,3 +644,10 @@ InstanceData* CreateInstanceData(Map* pMap)
     return pTempScript->GetInstanceData(pMap);
 }
 
+#ifdef WIN32
+#  include <windows.h>
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    return true;
+}
+#endif
