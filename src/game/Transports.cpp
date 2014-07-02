d957e2752fc415862ed6a604afdf4fc4922bcc15
Antz <billy1arm@users.noreply.github.com>
2014-07-02 11:05:50 +0100

    [Core] More project sync

--- src/game/Transports.cpp
+++ src/game/Transports.cpp
@@ -1,5 +1,8 @@
 /**
- * This code is part of MaNGOS. Contributor & Copyright details are in AUTHORS/THANKS.
+ * MaNGOS is a full featured server for World of Warcraft, supporting
+ * the following clients: 1.12.x, 2.4.3, 3.3.5a, 4.3.4a and 5.4.8
+ *
+ * Copyright (C) 2005-2014  MaNGOS project <http://getmangos.eu>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * World of Warcraft, and all World of Warcraft or Warcraft art, images,
+ * and lore are copyrighted by Blizzard Entertainment, Inc.
  */
 
 #include "Common.h"
@@ -109,7 +115,7 @@ void MapManager::LoadTransports()
         m_Transports.insert(t);
 
         for (std::set<uint32>::const_iterator i = mapsUsed.begin(); i != mapsUsed.end(); ++i)
-            m_TransportsByMap[*i].insert(t);
+            { m_TransportsByMap[*i].insert(t); }
 
         // If we someday decide to use the grid to track transports, here:
         t->SetMap(sMapMgr.CreateMap(mapid, t));
@@ -208,10 +214,10 @@ struct keyFrame
     float tFrom, tTo;
 };
 
-bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
+bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32>& mapids)
 {
     if (pathid >= sTaxiPathNodesByPath.size())
-        return false;
+        { return false; }
 
     TaxiPathNodeList const& path = sTaxiPathNodesByPath[pathid];
 
@@ -223,7 +229,7 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
         if (mapChange == 0)
         {
             TaxiPathNodeEntry const& node_i = path[i];
-            if (node_i.mapid == path[i+1].mapid)
+            if (node_i.mapid == path[i + 1].mapid)
             {
                 keyFrame k(node_i);
                 keyFrames.push_back(k);
@@ -253,7 +259,7 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
     // find the rest of the distances between key points
     for (size_t i = 1; i < keyFrames.size(); ++i)
     {
-        if ((keyFrames[i].node->actionFlag == 1) || (keyFrames[i].node->mapid != keyFrames[i-1].node->mapid))
+        if ((keyFrames[i].node->actionFlag == 1) || (keyFrames[i].node->mapid != keyFrames[i - 1].node->mapid))
         {
             keyFrames[i].distFromPrev = 0;
         }
@@ -268,7 +274,7 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
         {
             // remember first stop frame
             if (firstStop == -1)
-                firstStop = i;
+                { firstStop = i; }
             lastStop = i;
         }
     }
@@ -278,9 +284,9 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
     {
         int j = (i + lastStop) % keyFrames.size();
         if (keyFrames[j].node->actionFlag == 2)
-            tmpDist = 0;
+            { tmpDist = 0; }
         else
-            tmpDist += keyFrames[j].distFromPrev;
+            { tmpDist += keyFrames[j].distFromPrev; }
         keyFrames[j].distSinceStop = tmpDist;
     }
 
@@ -290,20 +296,20 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
         tmpDist += keyFrames[(j + 1) % keyFrames.size()].distFromPrev;
         keyFrames[j].distUntilStop = tmpDist;
         if (keyFrames[j].node->actionFlag == 2)
-            tmpDist = 0;
+            { tmpDist = 0; }
     }
 
     for (size_t i = 0; i < keyFrames.size(); ++i)
     {
         if (keyFrames[i].distSinceStop < (30 * 30 * 0.5f))
-            keyFrames[i].tFrom = sqrt(2 * keyFrames[i].distSinceStop);
+            { keyFrames[i].tFrom = sqrt(2 * keyFrames[i].distSinceStop); }
         else
-            keyFrames[i].tFrom = ((keyFrames[i].distSinceStop - (30 * 30 * 0.5f)) / 30) + 30;
+            { keyFrames[i].tFrom = ((keyFrames[i].distSinceStop - (30 * 30 * 0.5f)) / 30) + 30; }
 
         if (keyFrames[i].distUntilStop < (30 * 30 * 0.5f))
-            keyFrames[i].tTo = sqrt(2 * keyFrames[i].distUntilStop);
+            { keyFrames[i].tTo = sqrt(2 * keyFrames[i].distUntilStop); }
         else
-            keyFrames[i].tTo = ((keyFrames[i].distUntilStop - (30 * 30 * 0.5f)) / 30) + 30;
+            { keyFrames[i].tTo = ((keyFrames[i].distUntilStop - (30 * 30 * 0.5f)) / 30) + 30; }
 
         keyFrames[i].tFrom *= 1000;
         keyFrames[i].tTo *= 1000;
@@ -318,7 +324,7 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
     int t = 0;
     bool teleport = false;
     if (keyFrames[keyFrames.size() - 1].node->mapid != keyFrames[0].node->mapid)
-        teleport = true;
+        { teleport = true; }
 
     WayPoint pos(keyFrames[0].node->mapid, keyFrames[0].node->x, keyFrames[0].node->y, keyFrames[0].node->z, teleport,
                  keyFrames[0].node->arrivalEventID, keyFrames[0].node->departureEventID);
@@ -357,7 +363,7 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
                     //                    sLog.outString("T: %d, D: %f, x: %f, y: %f, z: %f", t, d, newX, newY, newZ);
                     WayPoint pos(keyFrames[i].node->mapid, newX, newY, newZ, teleport);
                     if (teleport)
-                        m_WayPoints[t] = pos;
+                        { m_WayPoints[t] = pos; }
                 }
 
                 if (tFrom < tTo)                            // caught in tFrom dock's "gravitational pull"
@@ -390,9 +396,9 @@ bool Transport::GenerateWaypoints(uint32 pathid, std::set<uint32> &mapids)
         }
 
         if (keyFrames[i + 1].tFrom > keyFrames[i + 1].tTo)
-            t += 100 - ((long)keyFrames[i + 1].tTo % 100);
+            { t += 100 - ((long)keyFrames[i + 1].tTo % 100); }
         else
-            t += (long)keyFrames[i + 1].tTo % 100;
+            { t += (long)keyFrames[i + 1].tTo % 100; }
 
         bool teleport = false;
         if ((keyFrames[i + 1].node->actionFlag == 1) || (keyFrames[i + 1].node->mapid != keyFrames[i].node->mapid))
@@ -434,7 +440,7 @@ void Transport::MoveToNextWayPoint()
 
     ++m_next;
     if (m_next == m_WayPoints.end())
-        m_next = m_WayPoints.begin();
+        { m_next = m_WayPoints.begin(); }
 }
 
 void Transport::TeleportTransport(uint32 newMapid, float x, float y, float z)
@@ -491,14 +497,14 @@ bool Transport::AddPassenger(Player* passenger)
 bool Transport::RemovePassenger(Player* passenger)
 {
     if (m_passengers.erase(passenger))
-        DETAIL_LOG("Player %s removed from transport %s.", passenger->GetName(), GetName());
+        { DETAIL_LOG("Player %s removed from transport %s.", passenger->GetName(), GetName()); }
     return true;
 }
 
 void Transport::Update(uint32 /*update_diff*/, uint32 /*p_time*/)
 {
     if (m_WayPoints.size() <= 1)
-        return;
+        { return; }
 
     m_timer = WorldTimer::getMSTime() % m_period;
     while (((m_timer - m_curr->first) % m_pathTime) > ((m_next->first - m_curr->first) % m_pathTime))
@@ -531,7 +537,7 @@ void Transport::Update(uint32 /*update_diff*/, uint32 /*p_time*/)
         m_nextNodeTime = m_curr->first;
 
         if (m_curr == m_WayPoints.begin())
-            DETAIL_FILTER_LOG(LOG_FILTER_TRANSPORT_MOVES, " ************ BEGIN ************** %s", GetName());
+            { DETAIL_FILTER_LOG(LOG_FILTER_TRANSPORT_MOVES, " ************ BEGIN ************** %s", GetName()); }
 
         DETAIL_FILTER_LOG(LOG_FILTER_TRANSPORT_MOVES, "%s moved to %f %f %f %d", GetName(), m_curr->second.x, m_curr->second.y, m_curr->second.z, m_curr->second.mapid);
     }
@@ -541,7 +547,7 @@ void Transport::UpdateForMap(Map const* targetMap)
 {
     Map::PlayerList const& pl = targetMap->GetPlayers();
     if (pl.isEmpty())
-        return;
+        { return; }
 
     if (GetMapId() == targetMap->GetId())
     {
@@ -566,7 +572,7 @@ void Transport::UpdateForMap(Map const* targetMap)
 
         for (Map::PlayerList::const_iterator itr = pl.begin(); itr != pl.end(); ++itr)
             if (this != itr->getSource()->GetTransport())
-                itr->getSource()->SendDirectMessage(&out_packet);
+                { itr->getSource()->SendDirectMessage(&out_packet); }
     }
 }
 
