5ef0b22ac9929631fa48cfe2b62644f95096ee88
Antz <billy1arm@users.noreply.github.com>
2014-09-09 14:49:52 +0100

    Corrected typo

--- sql/updates/Rel19/World/19004_creature_template_power.sql
+++ sql/updates/Rel19/World/19004_creature_template_power.sql
@@ -1,4 +1,4 @@
-ALTER TABLE db_version CHANGE COLUMN 19003_spell_proc_event required_19004_creature_template_power bit;
+ALTER TABLE db_version CHANGE COLUMN required_19003_spell_proc_event required_19004_creature_template_power bit;
 
 ALTER TABLE creature_template CHANGE ManaMultiplier PowerMultiplier float NOT NULL DEFAULT '1';
 ALTER TABLE creature_template CHANGE RegenerateHealth RegenerateStats tinyint(3) unsigned NOT NULL DEFAULT '3';
