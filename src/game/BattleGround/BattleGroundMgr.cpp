896feff6ce2ccb6a5ed26cce84baf8a9581a94a8
Foereaper <foereaper@hotmail.com>
2014-09-11 00:05:02 +0200

    Added base Battleground support for Eluna

--- src/game/BattleGround/BattleGroundMgr.cpp
+++ src/game/BattleGround/BattleGroundMgr.cpp
@@ -1563,6 +1563,10 @@ uint32 BattleGroundMgr::CreateBattleGround(BattleGroundTypeId bgTypeId, bool IsA
 
     // add bg to update list
     AddBattleGround(bg->GetInstanceID(), bg->GetTypeID(), bg);
+    
+#ifdef ENABLE_ELUNA
+    sEluna->OnBGCreate(bg, bgTypeId, bg->GetInstanceID());
+#endif /* ENABLE_ELUNA */
 
     // return some not-null value, bgTypeId is good enough for me
     return bgTypeId;
