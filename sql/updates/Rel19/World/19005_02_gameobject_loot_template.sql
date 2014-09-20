0f05c60898846540a66b65876c6d91e0fdd2ccdf
Antz <billy1arm@users.noreply.github.com>
2014-09-20 12:52:56 +0100

    Added missing where clause

--- sql/updates/Rel19/World/19005_02_gameobject_loot_template.sql
+++ sql/updates/Rel19/World/19005_02_gameobject_loot_template.sql
@@ -2,5 +2,5 @@
 DELETE FROM gameobject_loot_template WHERE entry=3239;
 
 -- Add the chest-loot with correct loot (Quest-Item, part of a Quest). :)
-UPDATE gameobject_loot_template SET entry=3239, item=4883, ChanceOrQuestChance=100, groupid=0, mincountOrRef=1, maxcount=1, condition_id=0;
+UPDATE gameobject_loot_template SET entry=3239, item=4883, ChanceOrQuestChance=100, groupid=0, mincountOrRef=1, maxcount=1, condition_id=0 WHERE entry=3239;
 -- I don't know why insert/update show's error (for me) but it works... :O
