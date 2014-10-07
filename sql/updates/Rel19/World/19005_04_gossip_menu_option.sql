2456253a842e680c833b0ec248f21e24fff570bb
Wolfi <info@xn--kfgames-5wa.de>
2014-10-07 17:06:58 +0200

    [DB] Fixing mistake here... <3

--- sql/updates/Rel19/World/19005_04_gossip_menu_option.sql
+++ sql/updates/Rel19/World/19005_04_gossip_menu_option.sql
@@ -4,4 +4,4 @@ ALTER TABLE db_version CHANGE COLUMN required_19005_03_creature_model_race requi
 UPDATE `gossip_menu_option` SET `option_text`='I wish to train my pet.' WHERE `menu_id`=4783 AND `ID`=3;
 
 -- This removes an unused/doubled Option in Shaman Trainer Menu.
-DELETE FROM `locales_gossip_menu_option` WHERE `menu_id`=4516 AND `id`=2;
+DELETE FROM `gossip_menu_option` WHERE `menu_id`=4516 AND `id`=2;
